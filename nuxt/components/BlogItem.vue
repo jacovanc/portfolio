88aced97141b4c1e4eb69bd74954ef557c0c0d16
Jaco <jacovancr@gmail.com>
2022-07-22 14:08:29 +0100

    Blogs page

---
+++ nuxt/components/BlogItem.vue
@@ -0,0 +1,58 @@
+<template>
+	<a v-if="BlogPost" :href="BlogPost.link" class="blog-item" target="_blank">
+		<h2 class="title">{{ BlogPost.title }}</h2>
+		<span>{{ BlogPost.platform }}</span> |
+		<span>{{ BlogPost.date }}</span>
+		<div class="description">
+			{{ BlogPost.description }}
+		</div>
+	</a>
+</template>
+
+<script>
+	export default {
+		props: {
+			BlogPost: {
+				type: Object,
+				required: true
+			}
+		},
+		created() {
+			console.log(this.BlogPost.link);
+		}
+	}
+</script>
+
+<style scoped>
+	a.blog-item,
+	a.blog-item:visited,
+	a.blog-item:hover,
+	a.blog-item:focus,
+	a.blog-item:hover, 
+	a.blog-item:active { 
+		text-decoration: none; color:inherit; 
+	}
+	.blog-item {
+		text-decoration: none;
+		font-size: 15px;
+		margin-bottom: 15px;
+		display: inline-block;
+
+		transition: 0.2s ease;
+
+		border-left: 1px solid grey;
+		padding-left: 10px;
+	}
+	.blog-item:hover {
+		transform: scale(1.01);
+	}
+	.blog-item .title {
+		margin: 0;
+	}
+	.blog-item span {
+		font-style: italic;
+	}
+	.blog-item .description {
+		margin-top: 3px;
+	}
+</style>
--- nuxt/pages/blog.vue
+++ nuxt/pages/blog.vue
@@ -1,24 +1,27 @@
 <template>
-  <div>
-    <NuxtLink to="/">Home</NuxtLink>
-  </div>
+  <div class="inner-wrapper blog" v-if="pageData">
+    <h1>{{ pageData.data.title }}</h1>
+    <div class="content" v-html="pageData.data.description">
+      
+    </div>
+    <div class="blog-items" v-if="blogs">
+      <BlogItem v-for="blog in blogs.data" :key="blog.id" :BlogPost="blog"/>
+    </div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "BlogsPage",
+  name: "blog",
   data() {
     return {
       pageData: null,
+      blogs: null,
     };
   },
   async fetch() { // Fetch used to pre-render data before build
-    this.pageData = await this.$axios.$get("/items/Homepage");
-  },
-  methods: {
-    async fetchNewData() {
-      console.log("hit");
-    }
+    this.pageData = await this.$axios.$get("/items/blog_category");
+    this.blogs = await this.$axios.$get("/items/blog");
   },
 };
 </script>
