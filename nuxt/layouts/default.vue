32268f24bcea4033f2a68b78262add84187429ae
Jaco <jacovancr@gmail.com>
2022-07-28 18:54:28 +0100

    Initial portfolio

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -2,10 +2,10 @@
 	<div class="main-wrapper" id="touchsurface">
 		<div id="nav">
 			<div class="inner-wrapper">
-        <NuxtLink to="/" class="site-title">Jaco van Cranenburgh</NuxtLink>
+       			 <NuxtLink to="/" class="site-title">Jaco van Cranenburgh</NuxtLink>
 				<div class="links">
-          <NuxtLink to="/">About</NuxtLink>
-					<!-- <router-link to="/portfolio">Portfolio</router-link> | -->
+         			<NuxtLink to="/">About</NuxtLink>
+					<router-link to="/portfolio">Portfolio</router-link>
 					<NuxtLink to="/blog">Blog</NuxtLink> 
 					<!-- <a :href="`${urlCV}`" target="_blank">CV</a> -->
 				</div>
@@ -13,7 +13,7 @@
 			</div>
 		</div>
 		<div class="outer-wrapper">
-      <Nuxt/>
+   		   <Nuxt/>
 			<!-- <div class="back-to-top" v-if="largeContent" v-on:click="backToTop"></div> -->
 		</div>
 	</div>
---
+++ nuxt/pages/portfolio.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="inner-wrapper blog" v-if="pageData">
+	  {{ pageData }}
+    <h1>{{ pageData.data.title }}</h1>
+    <div class="content" v-html="pageData.data.description">
+      
+    </div>
+    <div class="blog-items" v-if="portfolioItems">
+      
+    </div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "portfolio",
+  data() {
+    return {
+      pageData: null,
+      portfolioItems: null,
+    };
+  },
+  async fetch() { // Fetch used to pre-render data before build
+    this.pageData = await this.$axios.$get("/items/portfolio_category");
+    this.portfolioItems = await this.$axios.$get("/items/portfolio_item");
+  },
+};
+</script>
